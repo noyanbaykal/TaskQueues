5c693466b3bbbe92b4bd83a4922019f88f9ef74f
Noyan <noyanbaykal@gmail.com>
2020-08-19 13:30:48 -0400

    Refactored shared logic between Queue & Task into a CrudCard component

---
+++ src/components/CrudCard.js
@@ -0,0 +1,280 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Card, Icon, Label, Transition } from 'semantic-ui-react';
+
+function CreateButton({ onCreate }) {
+  const BACKGROUND_COLOR = 'rgb(239, 239, 239)';
+  const BACKGROUND_COLOR_HOVERED = 'rgb(210, 210, 210)';
+
+  const [color, setColor] = useState(BACKGROUND_COLOR);
+
+  return (
+    <Card style={{ textAlign: 'center', boxShadow: 'none'}}>
+      <button
+        onClick={onCreate}
+        style={{ margin: '0 auto', backgroundColor: color }}
+        onMouseEnter={() => setColor(BACKGROUND_COLOR_HOVERED)}
+        onMouseLeave={() => setColor(BACKGROUND_COLOR)}
+      >
+        <Icon className='plus circle'/>
+      </button>
+    </Card>
+  );
+};
+
+const KEY_CODE_ENTER = 13;
+const KEY_CODE_SPACE = 32;
+
+const SHOULD_DISPATCH = (keyCode) => {
+  return keyCode === KEY_CODE_ENTER || keyCode === KEY_CODE_SPACE;
+};
+
+const DISPLAY_MODES = Object.freeze({
+  NO_CONTENT: 'noContent',
+  EDIT: 'edit',
+  DISPLAY: 'display',
+  NEED_CONFIRMATION: 'needConfirmation',
+});
+
+const SHOULD_DISPLAY = (content) => {
+  return content ? DISPLAY_MODES.DISPLAY : DISPLAY_MODES.NO_CONTENT;
+};
+
+const getCardDisplayButtons = (editFunction, deleteFunction, specialButtonIcon, handleSpecial) => {
+  return (
+    <Card.Content extra>
+      <button onClick={editFunction} style={{ marginRight: '0.65em' }}>
+        <Icon className='pencil alternate' />
+      </button>
+      <button onClick={deleteFunction}>
+        <Icon className='trash' />
+      </button>
+      {
+        handleSpecial &&
+          <button className='right floated' onClick={handleSpecial}>
+            <Icon className={specialButtonIcon}/>
+          </button>
+      }
+    </Card.Content>
+  );
+};
+
+const getConfirmationButtons = (onConfirm, onCancel, confirmDisabled) => {
+  return (
+    <Card.Content extra>
+      <button className='left floated' onClick={onConfirm} disabled={confirmDisabled}>
+        <Icon className='check' />
+      </button>
+      <button className='right floated' onClick={onCancel}>
+        <Icon className='delete' />
+      </button>
+    </Card.Content>
+  );
+};
+
+function CrudCard(props) {
+  const {
+    children, content, componentName, cardTransition, getCardContent, getEditContent, getDisplayContent, DELETE_LABEL, cardStyle,
+    specialButtonIcon, specialButtonFunction, specialButtonAfterFunction, confirmDisabled, storeBeforeInput, resetAfterInput,
+    deleteHandler, modalHandler, enableClick, enableKeyboardClick,
+  } = props;
+
+  const [displayMode, setDisplayMode] = useState(SHOULD_DISPLAY(content));
+  const [showButtons, setShowButtons] = useState(false);
+
+  const onClickCreateOrEdit = () => {
+    storeBeforeInput();
+
+    setDisplayMode(DISPLAY_MODES.EDIT);
+  };
+  
+  const onClickDelete = () => {
+    setDisplayMode(DISPLAY_MODES.NEED_CONFIRMATION);
+  };
+  
+  const handleConfirm = () => {
+    modalHandler();
+
+    setDisplayMode(SHOULD_DISPLAY(content));
+  };
+  
+  const handleCancel = () => {
+    if(displayMode === DISPLAY_MODES.EDIT) {
+      resetAfterInput();
+    }
+
+    setDisplayMode(SHOULD_DISPLAY(content));
+  };
+
+  const handleDelete = () => {
+    setDisplayMode(SHOULD_DISPLAY(content));
+
+    deleteHandler();
+  };
+
+  const handleSpecial = () => {
+    if(!specialButtonIcon || !specialButtonFunction) {
+      return undefined;
+    }
+
+    specialButtonFunction();
+
+    if(specialButtonAfterFunction && cardTransition) {
+      const { duration } = cardTransition;
+
+      setTimeout(() => {
+        setDisplayMode(SHOULD_DISPLAY(content));
+  
+        specialButtonAfterFunction();
+      }, duration);
+    }
+  };
+
+  const composeCardContent = () => {
+    return (
+      <>
+        { 
+          getCardContent &&
+            getCardContent()
+        }
+        {
+          displayMode === DISPLAY_MODES.EDIT &&
+            <>
+              { getEditContent() }
+            </>
+        }
+        {
+          displayMode !== DISPLAY_MODES.EDIT &&
+            <>
+              { getDisplayContent() }
+            </>
+        }
+        {
+          displayMode === DISPLAY_MODES.NEED_CONFIRMATION &&
+            <Label>{ DELETE_LABEL }</Label>
+        }
+      </>
+    );
+  }
+
+  const getCardExtraContent = () => {
+    if(displayMode === DISPLAY_MODES.DISPLAY) {
+      if(!showButtons) {
+        return null;
+      }
+      
+      return getCardDisplayButtons(onClickCreateOrEdit, onClickDelete, specialButtonIcon, handleSpecial);
+    }
+  
+    if(displayMode === DISPLAY_MODES.EDIT) {
+      return getConfirmationButtons(handleConfirm, handleCancel, confirmDisabled);
+    }
+  
+    if(displayMode === DISPLAY_MODES.NEED_CONFIRMATION) {
+      return getConfirmationButtons(handleDelete, handleCancel);
+    }
+  
+    return null;
+  };
+
+  // Semantic applies a hover effect to cards with onClick handlers set.
+  // Unclickable cards should not have this effect.
+  const getOnClick = () => {
+    if(!enableClick) {
+      return undefined;
+    }
+
+    return function() {
+      handleClickCard();
+    }
+  };
+
+  const getOnKeyboardClick = (e) => {
+    if(!enableClick || !enableKeyboardClick) {
+      return undefined;
+    }
+
+    return function(e) {
+      if(e.target === e.currentTarget && SHOULD_DISPATCH(e.keyCode) && displayMode === DISPLAY_MODES.DISPLAY) {
+        handleClickCard();
+      } 
+    }
+  };
+
+  const handleClickCard = () => {
+    setShowButtons((showButtons) => !showButtons);
+  }
+
+  const getCard = () => {
+    return (
+      <Card
+        style={cardStyle}
+        onClick={getOnClick()}
+        tabIndex={enableClick && enableKeyboardClick && displayMode === DISPLAY_MODES.DISPLAY ? 0 : undefined}
+        onKeyDown={getOnKeyboardClick()}
+      >
+        { composeCardContent() }
+        { children }
+        { getCardExtraContent() }
+      </Card>
+    );
+  }
+
+  const showContent = () => {
+    if(cardTransition) {
+      const { type, duration, visible } = cardTransition;
+
+      return (
+        <Transition
+          animation={type}
+          duration={duration}
+          visible={visible}
+        >
+          { getCard() }
+        </Transition>
+      );
+    } else {
+      return getCard();
+    }
+  };
+
+  return (
+    <div className={`${componentName} ${displayMode}`}>
+      {
+        displayMode === DISPLAY_MODES.NO_CONTENT
+        ?
+          <CreateButton onCreate={onClickCreateOrEdit}/>
+        :
+          showContent()
+      }
+    </div>
+  );
+}
+
+CrudCard.propTypes = {
+  children: PropTypes.node,
+  content: PropTypes.object,
+  componentName: PropTypes.string.isRequired,
+  cardTransition: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    duration: PropTypes.number.isRequired,
+    visible: PropTypes.bool.isRequired,
+  }),
+  getCardContent: PropTypes.func,
+  getEditContent: PropTypes.func.isRequired,
+  getDisplayContent: PropTypes.func.isRequired,
+  DELETE_LABEL: PropTypes.string.isRequired,
+  cardStyle: PropTypes.object,
+  specialButtonIcon: PropTypes.string,
+  specialButtonFunction: PropTypes.func,
+  specialButtonAfterFunction: PropTypes.func,
+  confirmDisabled: PropTypes.bool,
+  storeBeforeInput: PropTypes.func.isRequired,
+  resetAfterInput: PropTypes.func.isRequired,
+  deleteHandler: PropTypes.func.isRequired,
+  modalHandler: PropTypes.func.isRequired,
+  enableClick: PropTypes.bool,
+  enableKeyboardClick: PropTypes.bool,
+};
+
+export default CrudCard;
